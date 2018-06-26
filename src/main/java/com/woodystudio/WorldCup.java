95ebbcb9aca76d048b4be5990ce3c09cf7e9e8dd
WoodyYan <colorguitar@hotmail.com>
2018-06-26 21:40:15 +0800

    initial commit

---
+++ src/main/java/com/woodystudio/WorldCup.java
@@ -0,0 +1,10 @@
+package com.woodystudio;
+
+public class WorldCup {
+}
+
+// as a team, i have my name, my accumulate points（积分）, my total goals. 10
+// as a competition, i have home team and visiting team, and i can compete then get the score. 10
+// as a group, i have 4 teams, i can run group game which will have 6 competitions. 20
+// as
+
